3b4ae2038c3b4152589069907fdc0f0f793ff1cf
Ksgaksbsye <naincyprajapati122008@gmail.com>
2025-02-18 20:59:32 +0530

    Update Must_join.py

--- RessoMusic/plugins/tools/Must_join.py
+++ RessoMusic/plugins/tools/Must_join.py
@@ -15,7 +15,7 @@ async def must_join_channel(app: Client, msg: Message):
         try:
             await app.get_chat_member(MUST_JOIN, msg.from_user.id)
         except UserNotParticipant:
-            if MUST_JOIN.isvishnu():
+            if MUST_JOIN.isalpha():
                 link = "https://t.me/" + MUST_JOIN
             else:
                 chat_info = await app.get_chat(MUST_JOIN)
